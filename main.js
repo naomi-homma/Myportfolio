a3b814a79c51b587f0d4791bf67edcc9c4ad4214
naomi_homma <hnm.n.1029@gmail.com>
2020-08-28 20:51:52 +0900

    Edit modal.js

--- main.js
+++ main.js
@@ -24,66 +24,6 @@ ScrollReveal().reveal('.fadeIn', {
   reset: false 
 });
 
-{
-  //modal
-  const modalOpen = document.querySelectorAll('.works_modal_open');
-  const modal = document.querySelectorAll('.works_modal_wrapper');
-  const modalCover = document.querySelectorAll('.works_modal_mask');
-  const html = document.documentElement;
-  const body = document.body;
-  
-  let modalCloseAction;
-  let dataModalOpen;
-  let targetModal;
-  
-  const TIMEOUT_SPEED = 500;
-  
-  for (let i = 0; i < modalOpen.length; i++) {     
-      //モーダルを消す処理
-      modalCloseAction = function(e) {
-        targetModal = e.currentTarget.closest('.works_modal_wrapper');
-        targetModal.classList.add('is_close');  
-        setTimeout(function(e) {
-          targetModal.classList.remove('is_open');
-          targetModal.classList.remove('is_close');
-        }, TIMEOUT_SPEED);
-      };
-      
-      // グレー部分をクリックでmodalを消す
-      const modalWrapClose = function() {
-           modalCover[i].addEventListener('click',function(e){
-             modalCloseAction(e);
-        },false);
-      };
-  
-      // modalを出す
-      const modalWrapOpen = function(e) {
-        dataModalOpen = e.currentTarget.getAttribute('data-modal-open');
-        for (let b = 0; b < modal.length; b++) {
-        
-          if (modal[b].getAttribute('data-modal') === dataModalOpen) {
-            modal[b].classList.add('is_open');
-            modalWrapClose();
-            return false;
-          }
-        }
-      };
-      
-      modalOpen[i].addEventListener('click', function(e) {
-        modalWrapOpen(e);
-      }, false);
-  }
-  
-  // modalを消す
-  const modalBtnClose = document.querySelectorAll('.works_modal_close');
-  for (let n = 0; n < modalBtnClose.length; n++) {
-      modalBtnClose[n].addEventListener('click', function(e) {
-        modalCloseAction(e);
-        return false;
-      }, false);
-  }
-}
-
 // スムーズスクロール&ハンバーガーメニュー処理
 $(function() {
   //スムーズスクロール
---
+++ modules/modal.js
@@ -0,0 +1,6 @@
+//modalを開く関数："is_open"classをaddする
+//modalが複数ある⇒data属性
+const modalWrapOpen = function(e) {
+  //イベントが付けられたノードに対してdata属性(modal-open)をセット
+  const dataModalOpen = e.currentTarget.dataset.modalOpen;
+}
