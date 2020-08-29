24e27c3293e3ccea966970d52f6981497b0563f2
naomi_homma <hnm.n.1029@gmail.com>
2020-08-29 18:40:59 +0900

    Edit JS of modal

--- modules/modal.js
+++ modules/modal.js
@@ -3,4 +3,13 @@
 const modalWrapOpen = function(e) {
   //イベントが付けられたノードに対してdata属性(modal-open)をセット
   const dataModalOpen = e.currentTarget.dataset.modalOpen;
+  Array.from(document.querySelectorAll('.work_modal_wrapper')).forEach((e, i) => {
+    if(e.getAttribute('data-modal') === dataModalOpen){
+      e.classList.toggle('is_open');
+    }
+  })
 }
+
+Array.from(document.querySelectorAll('.work_modal_open')).forEach((modalOpenElement) => {
+  modalOpenElement.addEventListener('click', modalWrapOpen);
+})
