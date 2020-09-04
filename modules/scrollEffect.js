5ef0af0813af806410f2df0da9b2e5d98c787a02
naomi_homma <hnm.n.1029@gmail.com>
2020-09-04 12:24:51 +0900

    Edit scrollEffect.js

--- modules/scrollEffect.js
+++ modules/scrollEffect.js
@@ -1,14 +1,21 @@
-const scrollEffect = () => {
-  const targetElement = document.getElementsByClassName('js_fadeIn');
-  if(!targetElement) return;
+const scrollEffect = (e) => {
+  const targetElements = document.getElementsByClassName('js_fadeIn');
+  if(!targetElements) return;
 
-  for (let i=0; i < targetElement.length; i++) {
-    const showTiming = targetElement[i].clientHeight * 0.6;
-    const getElementDistance = targetElement[i].getBoundingClientRect().top + showTiming;
-    //要素の高さの60%が画面に出現したらis_showが付与される
+  // for (let i=0; i < targetElement.length; i++) {
+  //   const showTiming = targetElement[i].clientHeight * 0.6;
+  //   const getElementDistance = targetElement[i].getBoundingClientRect().top + showTiming;
+  //   //要素の高さの60%が画面に出現したらis_showが付与される
+  //   if(window.innerHeight > getElementDistance) {
+  //     targetElement[i].classList.add('is_show');
+  //   }
+  // }
+  Array.from(targetElements).forEach((targetElement) => {
+    const showTiming = targetElement.clientHeight * 0.6;
+    const getElementDistance = targetElement.getBoundingClientRect().top + showTiming;
     if(window.innerHeight > getElementDistance) {
-      targetElement[i].classList.add('is_show');
+      targetElement.classList.add('is_show');
     }
-  }
+  })
 }
 window.addEventListener('scroll', scrollEffect);
